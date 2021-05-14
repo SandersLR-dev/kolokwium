2ec57d2401f77241fb2fb5bad50ed5dd395734b1
Dominik Snopek <222572@edu.p.lodz.pl>
2021-05-14 16:21:02 +0200

    test dont using heater

--- testsReactor/src/test/java/edu/iis/mto/oven/OvenTest.java
+++ testsReactor/src/test/java/edu/iis/mto/oven/OvenTest.java
@@ -1,18 +1,43 @@
 package edu.iis.mto.oven;
 
+import static edu.iis.mto.oven.Oven.HEAT_UP_AND_FINISH_SETTING_TIME;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class OvenTest {
 
+    @Mock
+    private Fan fanMock;
+
+    @Mock
+    private HeatingModule heatingModuleMock;
+
     @Test
-    void itCompiles() {
-        MatcherAssert.assertThat(true, equalTo(true));
+    void shouldDontUseHeater() {
+
+        Oven oven = new Oven(heatingModuleMock,fanMock);
+
+        BakingProgram bakingProgram=BakingProgram.builder().build();
+
+
+        oven.start(bakingProgram);
+
+        verify(heatingModuleMock, times(0)).heater(HeatingSettings.builder()
+                .withTargetTemp(bakingProgram.getInitialTemp())
+                .withTimeInMinutes(HEAT_UP_AND_FINISH_SETTING_TIME)
+                .build());
+
+
     }
 
 }
